472b3783510b230aa7c2e87dbf763af4cdfe43f4
Задорин Артем <artfan95@gmail.com>
2024-11-22 16:54:15 +0700

    Правки стилей

--- src/App/Component/Header/Header.js
+++ src/App/Component/Header/Header.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./Header.scss"
 import { Button, Typography } from 'antd';
-const { Link } = Typography;
-
 
 export default function Header() {
   return (
     <header className="header">
       <div className="container header__content">
-        <a href="/" className="header__logo">ЦСКН1</a>
+        <Button type="link" href="/" className="header__logo">ЦСКН1</Button>
         <div className="header__controls">
           <Button type="link" size="large">Скачать презентацию</Button>
           <Button type="primary" size="large" href="">Написать основателю</Button>
