27c7169316e536d9263a00a1d7f4e82f8bebc8e6
Задорин Артем <artfan95@gmail.com>
2025-06-12 23:40:37 +0700

    Доработки адаптивности шестого блока

--- src/App/Component/Six/Six.js
+++ src/App/Component/Six/Six.js
@@ -1,107 +1,7 @@
 import React from 'react';
 import { Typography, Button } from 'antd';
 const { Title } = Typography;
-import Tile from '../../Shared/Tile';
 import './Six.scss';
-
-const reality = [
-	{
-		icon: 'camera',
-		title: 'Описание концепции шоу',
-		steps: [
-			'Реальные истории людей',
-			'Процесс трансформации',
-			'Драматургия и обучение',
-			'Вирусное распространение',
-		],
-	},
-	{
-		icon: 'vector',
-		title: 'Каналы дистрибуции',
-		steps: [
-			'YouTube, RuTube, VK Video',
-			'Федеральные телеканалы',
-			'Стриминговые сервисы',
-			'Социальные сети',
-		],
-	},
-	{
-		icon: 'target',
-		title: 'Прогноз охватов',
-		steps: [
-			'Год 1: 5+ млн просмотров',
-			'Год 2: 25+ млн просмотров',
-			'Год 3: 100+ млн просмотров',
-		],
-	},
-];
-
-const digital = [
-	{
-		title: 'Awareness',
-		steps: ['Контент-маркетинг', 'PR и медиа', 'Influencer marketing'],
-	},
-	{
-		title: 'Interest',
-		steps: [
-			'Экспертный контент',
-			'Case studies',
-			'Социальные доказательства',
-		],
-	},
-	{
-		title: 'Consideration',
-		steps: ['Персонализированный контент', 'Ретаргетинг', 'CRM-маркетинг'],
-	},
-	{
-		title: 'Action',
-		steps: [
-			'Триггерные коммуникации',
-			'Автоматизированные продажи',
-			'Программы лояльности',
-		],
-	},
-];
-
-const strategy = [
-	{
-		icon: 'book',
-		title: 'Образовательный',
-		steps: [
-			'Разборы клиентских кейсов',
-			'Гайды по личным финансам',
-			'Чек-листы и инструкции',
-		],
-	},
-	{
-		icon: 'play',
-		title: 'Развлекательный',
-		steps: ['Истории успеха', 'Behind the scenes', 'Интервью с экспертами'],
-	},
-	{
-		icon: 'up',
-		title: 'Вовлекающий',
-		steps: ['Опросы и тесты', 'Челленджи', 'UGC-кампании'],
-	},
-];
-
-const tech = [
-	{
-		icon: 'toolbox',
-		title: 'Инструменты',
-		steps: [
-			'AI-генерация креативов',
-			'Предиктивная аналитика',
-			'Персонализация контента',
-			'Автоматическая оптимизация',
-		],
-	},
-	{
-		icon: 'check',
-		title: 'Ключевые метрики',
-		steps: ['CAC: 300₽', 'LTV: 20,000₽', 'ROI: 460%', 'Retention: 70%'],
-	},
-];
 
 export default function Six() {
 	return (
@@ -413,13 +313,28 @@ export default function Six() {
 							<p className="six-year__item six-year__item--result"></p>
 						</div>
 					</div>
-          <h4 className='six__subtitle'>
-Цель за 6 лет занять <span className='six__subtitle--green'>20%</span> доли рынка
-          </h4>
-          <div className="six__controls">
-                    <Button size="large" href="https://docs.google.com/spreadsheets/d/1NEAe3V-I57E4UTDDM5yDIrP9ECTtxyktfqydFMAaFrY/edit?usp=sharing" target='_blank'>Посмотреть финмодель</Button>
-                    <Button type="primary" size="large" href="https://wa.me/message/CG4TEM4HZBQQA1" target="_blank">Написать основателю</Button>
-                  </div>
+					<h4 className="six__subtitle">
+						Цель за 6 лет занять{' '}
+						<span className="six__subtitle--green">20%</span> доли
+						рынка
+					</h4>
+					<div className="six__controls">
+						<Button
+							size="large"
+							href="https://docs.google.com/spreadsheets/d/1NEAe3V-I57E4UTDDM5yDIrP9ECTtxyktfqydFMAaFrY/edit?usp=sharing"
+							target="_blank"
+						>
+							Посмотреть финмодель
+						</Button>
+						<Button
+							type="primary"
+							size="large"
+							href="https://wa.me/message/CG4TEM4HZBQQA1"
+							target="_blank"
+						>
+							Написать основателю
+						</Button>
+					</div>
 				</div>
 			</div>
 		</section>
